a193d87a095f8305948240dcc7d0dc5b285923de
Xiao Wang <24860335+xwang233@users.noreply.github.com>
2023-03-08 20:49:10 -0800

    add sample lint job

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+
+import sys
+import re, math
+
+
+def a_function():
+    a = 3
+    b    = 4
+    c =     5
+
+    d = a    + b
+
+
+
+
+    f = 3
+    if c == 4: d = 5
+
+    for    i in    range(15,30,2  ):
+        j = math.pow(a,b, 3)
+    
+class SomeClass:
+    def __init__(self, a = 3):
+        self.s: str
+    
+    def add(self, b:   int=4):
+        b += self.s
+
+sc = SomeClass()
+sc.add(c)
+
+ls = [
+    SomeClass(a = i) for i
+                                in range(1,10,2)
+]
+
